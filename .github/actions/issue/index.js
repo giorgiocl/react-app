8bb319927cfaa9e081bb1a9f11af595d76cb8aee
giorgiocl <geovanny.campoverde@thoughtworks.com>
2021-10-11 16:33:25 -0500

    fix: fixing action compiled

--- .github/actions/issue/index.js
+++ .github/actions/issue/index.js
@@ -8,7 +8,7 @@ try {
   const assignees = core.getInput("assignees");
 
   const octokit = new github.getOctokit(token);
-  const response = await octokit.rest.issues.create({
+  const response = octokit.rest.issues.create({
     //owner: github.context.repo.owner,
     //repo: github.context.repo.repo,
     ...github.context.repo,
