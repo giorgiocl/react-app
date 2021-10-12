911a26467e05e2c56af3f64cc62c26ad445e4b39
giorgiocl <geovanny.campoverde@thoughtworks.com>
2021-10-12 09:28:25 -0500

    fix: fixing name attribute in sh script

--- .github/actions/hello-docker/entrypoint.sh
+++ .github/actions/hello-docker/entrypoint.sh
@@ -8,7 +8,7 @@ echo "::error ::Error Message"
 echo "::add-mask::$1"
 echo "Hello $1"
 time=$(date)
-echo "::set-output nama=time::$time"
+echo "::set-output name=time::$time"
 
 echo "::group::Some expandable logs"
 echo "Some stuff"
